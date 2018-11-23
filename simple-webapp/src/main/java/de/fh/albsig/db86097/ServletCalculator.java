9942d8c44cbae5cb8cf3f3a4716e21d55ce9e153
SouthDB <38219134+SouthDB@users.noreply.github.com>
2018-11-23 23:00:34 +0100

    fix exception handle in servlet

--- simple-webapp/src/main/java/de/fh/albsig/db86097/ServletCalculator.java
+++ simple-webapp/src/main/java/de/fh/albsig/db86097/ServletCalculator.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 import org.json.simple.JSONObject;
 
@@ -21,11 +22,12 @@ public class ServletCalculator extends HttpServlet {
     final static Logger logger = Logger.getLogger(HttpServlet.class);
 
     /**
-     * handle post request for Caclculator and start fibonacci or PowerOfTwo
+     * handle post request for Calculator and start fibonacci or PowerOfTwo
      * mode.
      * 
-     * @param request  inlcude mode and n from client
-     * @param response send result or calculate error to client
+     * @param request       include mode and n from client
+     * @param response      send result or calculate error to client
+     * @param numberContain check n only contains numbers
      * @throws ServletException Servlet error for client
      * @throws IOException      failed or interrupted I/O operations
      */
@@ -37,52 +39,67 @@ public class ServletCalculator extends HttpServlet {
         String mode = request.getParameter("mode");
         String reqN = request.getParameter("n");
 
+        boolean numberContain = StringUtils.containsOnly(reqN, '1', '2', '3',
+                '4', '5', '6', '7', '8', '9', '0');
+
         int n = 0;
         long result = 0;
         String reqResult = null;
 
-        // covert request Parameter
-        try {
-            n = Integer.parseInt(reqN);
-        } catch (NumberFormatException nfe) {
-            logger.error("Parse client variable n goes wrong, nfe");
-            nfe.printStackTrace();
-            reqN = null;
+        // convert request Parameter
+        if (numberContain) {
+            try {
+                n = Integer.parseInt(reqN);
+            } catch (NumberFormatException nfe) {
+                logger.error("Parse client variable n goes wrong", nfe);
+                reqN = null;
+            }
         }
-        // start calculator mode
-        if (mode.equals("fibonacci") && reqN != null) {
-            Fibonacci fib = new Fibonacci();
-            result = fib.fibonacci(n);
 
-            if (result < 0) {
-                reqResult = "Input Error";
-            } else {
-                reqResult = "Result = " + Long.toString(result);
-            }
-        } else if (mode.equals("powTwo") && reqN != null) {
-            PowerOfTwo pt = new PowerOfTwo();
-            result = pt.powTwo(n);
+        try {
+            // start calculator mode
+            if (mode.equals("fibonacci") && reqN != null) {
+                Fibonacci fib = new Fibonacci();
+                result = fib.fibonacci(n);
 
-            if (result < 0) {
-                reqResult = "Input Error";
+                if (result < 0) {
+                    reqResult = "Input Error";
+                } else {
+                    reqResult = "Result = " + Long.toString(result);
+                }
+            } else if (mode.equals("powTwo") && reqN != null) {
+                PowerOfTwo pt = new PowerOfTwo();
+                result = pt.powTwo(n);
+
+                if (result < 0) {
+                    reqResult = "Input Error";
+                } else {
+                    reqResult = "Result = " + Long.toString(result);
+                }
             } else {
-                reqResult = "Result = " + Long.toString(result);
+                reqResult = "Parameter Error";
             }
-        } else {
-            reqResult = "Parameter Error";
+        } catch (Exception ex) {
+            logger.error("Calculate mode goes wrong", ex);
+            response.sendError(500);
         }
 
         // Json Response
-        Map<String, Object> forJsonObj = new HashMap<>();
-        forJsonObj.put("result", reqResult);
+        try {
+            Map<String, Object> forJsonObj = new HashMap<>();
+            forJsonObj.put("result", reqResult);
+            JSONObject jobj = new JSONObject(forJsonObj);
 
-        JSONObject jobj = new JSONObject(forJsonObj);
-        response.setContentType("application/json; charset=utf-8");
-        PrintWriter out = response.getWriter();
-        out.write(jobj.toString());
+            response.setContentType("application/json; charset=utf-8");
+            PrintWriter out = response.getWriter();
+            out.write(jobj.toString());
 
-        out.flush();
-        out.close();
+            out.flush();
+            out.close();
+        } catch (Exception ex) {
+            logger.error("JSON Response goes wrong", ex);
+            response.sendError(500);
+        }
 
     }
 }
