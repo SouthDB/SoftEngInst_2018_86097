265a9dd1b6e182b384a70a307cd61b7931633ad8
SouthDB <38219134+SouthDB@users.noreply.github.com>
2018-11-23 23:46:56 +0100

    add junit lifecycle

--- simple-webapp/src/test/java/de/fh/albsig/db86097/ServletCalculatorTest.java
+++ simple-webapp/src/test/java/de/fh/albsig/db86097/ServletCalculatorTest.java
@@ -1,10 +1,8 @@
 package de.fh.albsig.db86097;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
@@ -13,25 +11,57 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.junit.Test;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnitRunner;
 
+@RunWith(MockitoJUnitRunner.class)
 public class ServletCalculatorTest {
 
-	@Test
-	public void ServletTestFibonacci() throws Exception {
-		HttpServletRequest request = mock(HttpServletRequest.class);
-		HttpServletResponse response = mock(HttpServletResponse.class);
+    @Mock
+    private HttpServletRequest request;
 
-		when(request.getParameter("mode")).thenReturn("fibonacci");
-		when(request.getParameter("n")).thenReturn("6");
+    @Mock
+    private HttpServletResponse response;
 
-		StringWriter stringWriter = new StringWriter();
-		PrintWriter writer = new PrintWriter(stringWriter);
-		when(response.getWriter()).thenReturn(writer);
+    @BeforeAll
+    public void initExternalResources() {
+        // for example create entityManagerFacotory
+    }
 
-		new ServletCalculator().doPost(request, response);
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
 
-		verify(request, times(1)).getParameter("mode");
-		assertTrue(stringWriter.toString().contains("Result = 8"));
-	}
+    @Test
+    public void ServletTestFibonacci() throws Exception {
+        Mockito.when(request.getParameter("mode")).thenReturn("fibonacci");
+        Mockito.when(request.getParameter("n")).thenReturn("6");
 
+        StringWriter stringWriter = new StringWriter();
+        PrintWriter writer = new PrintWriter(stringWriter);
+        Mockito.when(response.getWriter()).thenReturn(writer);
+
+        new ServletCalculator().doPost(request, response);
+
+        verify(request, times(1)).getParameter("mode");
+        assertTrue(stringWriter.toString().contains("Result = 8"));
+    }
+
+    @AfterEach
+    public void clearDatabase() {
+        // for example delete created entries
+    }
+
+    @AfterAll
+    public void closeDatabase() {
+        // for example close connection to database
+    }
 }
